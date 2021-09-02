5a214b5be320c720598cf64dee8a336b4f9c9e78
kenriortega <kenriortega@gmail.com>
2021-09-02 16:03:25 -0400

    remove: unuse variables

--- pkg/errors/commons.go
+++ pkg/errors/commons.go
@@ -1,7 +1,1 @@
 package errors
-
-var (
-	// errors configs
-	ErrReadConfig      = NewError("config: error to load yaml file")
-	ErrUnmarshalConfig = NewError("config: error to unmarsahl yaml file")
-)
